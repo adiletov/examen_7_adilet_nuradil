902ecdc57e4cd1f27cf1b51c15d1c7d11e2d5267
Nuradil Adiletov <nrdl777@gmail.com>
2019-11-23 17:24:38 +0600

    add components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import OrderDetails from './Components/OrderDetails/OrderDetails';
+import Items from "./Components/Items/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    items: [
+      {name: 'Burger', price: 80, count: 0},
+      {name: 'Cheeseburger', price: 90, count: 0},
+      {name: 'Fries', price: 45, count: 0},
+      {name: 'Frappe', price: 70, count: 0},
+      {name: 'Tea', price: 50, count: 0},
+      {name: 'Cola', price: 40, count: 0},
+    ],
+    total: 0,
+  };
+
+  addFastFood = (itemIndex) => {
+    console.log(itemIndex)
+    let items = [...this.state.items];
+    let total = this.state.total;
+    const productIndex = items.findIndex(index => index.name === itemIndex.name);
+    items[productIndex].count++;
+    total += itemIndex.price;
+    this.setState({items, total});
+  };
+  removeFastFood = (itemIndex) => {
+    console.log(itemIndex);
+    let items = [...this.state.items];
+    let total = this.state.total;
+    const productIndex = items.findIndex(index => index.name === itemIndex.name);
+    items[productIndex].count--;
+    total -= itemIndex.price;
+    this.setState({items, total});
+    console.log(this.state.total)
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Items
+          addFastFood={this.addFastFood}
+        />
+        <OrderDetails
+            productsApp={this.state.items}
+            clickRemove={this.removeFastFood}
+            totalPriceApp={this.state.total}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/OrderDetails/OrderDetails.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import S from './style.module.css'
+
+const Product = [
+  {name: 'Burger',price: 80},
+  {name: 'Cheeseburger',price: 90},
+  {name: 'Fries',price: 45},
+  {name: 'Frappe',price: 70},
+  {name: 'Tea',price: 50},
+  {name: 'Cola',price: 40},
+];
+
+const  OrderDetails = (props) => {
+  console.log(props)k
+  return (
+    <div className={S.blockItems}>
+      {Product.map((product, index) =>
+        <div key={index} className={S.item}>
+          {props.productsApp[index].count > 0 ? <div>
+            <span>{product.name}</span>
+            <span> x {props.productsApp[index].count} шт</span>
+            <button className={S.btn}
+              onClick={() => props.clickRemove(product)}
+            >X
+            </button>
+          </div> : null
+          }
+        </div>
+      )}
+      <div><b>Итого : {props.totalPriceApp} сом</b></div>
+    </div>
+  );
+};
+
+export default OrderDetails;
